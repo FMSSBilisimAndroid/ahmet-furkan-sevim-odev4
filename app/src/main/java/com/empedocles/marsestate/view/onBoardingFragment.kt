0a81c39f3f92095dfb346695931d622bd22e9c47
Ahmet Furkan Sevim <ahmetfurkansevim5.gmail.com>
2022-09-16 16:17:51 +0300

    Add onBoarding page

--- app/src/main/java/com/empedocles/marsestate/view/onBoardingFragment.kt
+++ app/src/main/java/com/empedocles/marsestate/view/onBoardingFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.empedocles.marsestate.R
 import com.empedocles.marsestate.databinding.FragmentOnBoardingBinding
 import com.empedocles.marsestate.databinding.FragmentProductBinding
@@ -19,5 +20,9 @@ class onBoarding : Fragment() {
         binding = FragmentOnBoardingBinding.inflate(inflater)
         return binding.root
     }
-    
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.apply {  }
+    }
+
 }
